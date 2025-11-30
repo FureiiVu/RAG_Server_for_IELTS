0786540966d965688b6f9891ce56dd2ea19e90a2
FureiiVu <fureiivu9653@example.com>
2025-11-30 13:29:08 +0700

    Able to retrieve chunks and generate answer from LLM

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -5,13 +5,9 @@ dotenv.config();
 
 const weaviateEndpoint = process.env.WEAVIATE_ENDPOINT;
 const weaviateApiKey = process.env.WEAVIATE_ADMIN_API_KEY;
-const studioApiKey = process.env.GOOGLE_API_KEY;
 
 const weaviateClient = await weaviate.connectToWeaviateCloud(weaviateEndpoint, {
   authCredentials: new ApiKey(weaviateApiKey),
-  headers: {
-    "X-Goog-Studio-Api-Key": studioApiKey,
-  },
 });
 
 export default weaviateClient;
--- backend/controllers/retrievalController.js
+++ backend/controllers/retrievalController.js
@@ -1,26 +1,49 @@
-import { generativeParameters } from "weaviate-client";
+import dotenv from "dotenv";
+import { GoogleGenAI } from "@google/genai";
+
 import weaviateClient from "../config/connectDB.js";
+
+dotenv.config();
 
 export const handleTestRetrieval = async (req, res) => {
   try {
+    const { question } = req.body;
+
+    // 1. Retrieval
     const finalChunks = weaviateClient.collections.get("FinalChunksCollection");
-    const response = await finalChunks.generate.nearText(
-      "RAG",
-      {
-        groupedTask: "What is RAG?",
-        config: generativeParameters.google({
-          modelId: "gemini-1.5-flash",
-          temperature: 0.2,
-        }),
-      },
-      {
-        limit: 1,
-      }
-    );
-    res
-      .status(200)
-      .json({ message: "Test retrieval endpoint is working!", data: response });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+    const retrieved = await finalChunks.query.nearText("What is RAG?", {
+      limit: 5,
+    });
+
+    console.log("Retrieved objects:", retrieved.objects);
+
+    const contexts = retrieved.objects
+      .map((obj) => obj.properties.content)
+      .join("\n\n");
+
+    // 2. Gửi prompt sang Gemini
+    const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
+
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: `
+    You are an AI assistant.
+    Use the context below to answer the question.
+
+    --- CONTEXT ---
+    ${contexts}
+
+    --- QUESTION ---
+    ${question}
+
+    Answer:`,
+    });
+
+    res.json({
+      answer: response.text,
+      retrieved_chunks: contexts,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
